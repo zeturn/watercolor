b17fa2a635d9f65406ecd07755583c7198726739
henry <zhr626@outlook.com>
2025-06-29 13:55:46 +0800

    更新 package.json，优化 Vue 和 React 的单元测试命令，指定配置文件以提升测试灵活性和可维护性。

---
+++ vitest.vue.config.ts
@@ -0,0 +1,19 @@
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.js'],
+    include: ['tests/components/**/*.test.js'],
+    exclude: ['tests/components/**/*React.test.jsx'],
+  },
+}); 
