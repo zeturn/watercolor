8ef6f4fcf7533fbf874124acaeb124ce07e89fe5
Henry Zhao <62530500+zeturn@users.noreply.github.com>
2026-01-17 14:22:02 -0800

    Fix docs home route

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
+import HomePage from './HomePage.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    if (DefaultTheme.enhanceApp) {
+      DefaultTheme.enhanceApp({ app })
+    }
+    app.component('HomePage', HomePage)
+  },
+}
